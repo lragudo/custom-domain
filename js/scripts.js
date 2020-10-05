d4a9452c079ec4c81177703517051223706de625
Lillian <59583025+lragudo@users.noreply.github.com>
2020-10-05 16:41:36 -0700

    Added portfolio sections
    *added print and illustation
    *added color specific styling for both sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-// variables
-const triggerMotion = document.querySelector('.motionButton');
-const targetMotion = document.querySelector('.motion');
+// VARIABLES
+// web
 const triggerWeb = document.querySelector('.webButton');
 const targetWeb = document.querySelector('.web');
+//motion
+const triggerMotion = document.querySelector('.motionButton');
+const targetMotion = document.querySelector('.motion');
+// print
+const triggerPrint = document.querySelector('.printButton');
+const targetPrint = document.querySelector('.print');
+// illustation
+const triggerIllus = document.querySelector('.illusButton');
+const targetIllus = document.querySelector('.illustration');
 
-// on click
+// ON CLICK
+// web
+triggerWeb.addEventListener('click', () => {
+    targetWeb.classList.toggle('do-this');
+  });
+//   motion
 triggerMotion.addEventListener('click', () => {
   targetMotion.classList.toggle('do-this');
 });
-
-triggerWeb.addEventListener('click', () => {
-  targetWeb.classList.toggle('do-this');
+// print
+triggerPrint.addEventListener('click', () => {
+  targetPrint.classList.toggle('do-this');
 });
+// illustation
+triggerIllus.addEventListener('click', () => {
+    targetIllus.classList.toggle('do-this');
+  });
