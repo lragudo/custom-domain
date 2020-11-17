c11aee9ffc22e76078aae41538acaa81522ac6ac
Lillian <59583025+lragudo@users.noreply.github.com>
2020-11-17 15:22:28 -0800

    added React and Object lists

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// {id: 1, title: "", tagline: "", client:"", date:"", tools:"", deliverables:"", description:"", thumb: "/images/web/***/thumb.jpg", img1:"", img2:"", img3:"", portpage:"*****.html", link1:"", link2:"", link3:""}
+var webLists = [
+    {id: 1, title: "Google Doodle", tagline: "SVG Animation", client:"Self Initiated", date:"Febuary 2020 - March 2020", tools:"Codepen, VSCode, & Github", deliverables:"A SVG animation for a website", description:"A google doodle for International Tabletop Day. Animation was created using GSAP JS code.", thumb: "\images\web\google\thumb.jpg", img1:"", img2:"", img3:"", portpage:"googleDoodle.html", link1:"https://lragudo.github.io/google-doodle/", link2:"https://github.com/lragudo/google-doodle", link3:"https://codepen.io/collection/nYZaKm"},
+    {id: 2, title: "Soundboard",  tagline: "", client:"Self Initiated", date:"October 2020", tools:"Codepen, VSCode, & Github", deliverables:"Website", description:"", thumb: "", img1:"", img2:"", img3:"", portpage: "soundboard.html", link1:"https://lillianrosedesign-soundboard.netlify.app/"},
+    {id: 3, title: "Eclectic Outlet", tagline: "Digital instructions for a tabletop game", client:"Self Initiated", date:"January 2020 - Febuary 2020", tools:"Illustrator, Indesign, Figma, & Protopie", deliverables:"Cards and Mobile App", description:"I love playing tabletop games with friends and family. It’s an easy way for me to connect with others. However, your first time playing a new game can be difficult and the instructions are often long and complicated. I was inspired by the creators of Exploding Kittens and their animated instructions for all of their games. \n Eclectic Outlet is a card game that is fun and quick for highscool and college aged people. This game is accessible to anyone who wants to play it by being downloadable to print and the instructions are online.", thumb: "//via.placeholder.com/250/?text=Google Doodle", img1:"", img2:"", img3:"", portpage: "eclecticOutlet.html", link1:"https://cloud.protopie.io/p/b586671520", link2:"https://www.figma.com/file/r5Exjj66ltDlgU5Jdq0mMx/Card-Game-Instructions?node-id=0%3A1"},
+    {id: 4, title: "CoLab website", tagline: "A visual identity and website for The EWU DESN CoLab.", client:"EWU Design Department", date:"September - December 2020", tools:"Milanote, Figma, & Codepen", deliverables:"Website", description:"", thumb: "//via.placeholder.com/250/?text=Google Doodle", img1:"", img2:"", img3:"", portpage: "colab.html", link1:"", link2:""}
+  ];
+
+var printLists = [
+    {id: 1, title: "Spokane Guide", tagline: "A guide that assists MFWI students navigate Spokane.", client:"Mukogawa U.S. Campus", date:"Febuary 2018 - September 2019", tools:"Illustrator & Indesign", deliverables:"Paper guide and digital images", description:"I worked for Mukogawa US Campus (formerly known as Mukogawa Fort Write Institute) as one of their resident assistants for 2 years. In total I had 6 groups of 10- 12 students. My main job at the beginning of my student’s stay, was to show them around Spokane, how to use Spokane Transit, and plan outings with them. During my first session I was flooded with tons of questions about bus stops, bus routes, where Walmart was, and just general confusion about how to navigate Spokane. Students often asked about my favorite restaurants or stores to shop at as well. The students received bus route maps as well as a map of Spokane, since almost all students do not have a data plan on their phones and rarely can access the internet when off campus. These quides did not fit the needs of the Mukogawa students, so I decided to design a guide to handout to my students that would assist them during their time in Spokane. /n The guide concisely contains information about Spokane, the bus system, and outings that is only relevant to the Mukogawa students. This is to help reduce the amount of information the students have to analyze and understand for what is often their first trip to an English speaking country.", thumb: "/images/print/spokaneguide/thumb.jpg", img1:"", img2:"", img3:"", portpage:"spokaneGuide.html", link1:"https://issuu.com/lillian.ragudo/docs/travelguide_lillianragudoprint", link2:"", link3:""},
+    {id: 2, title: "Parade of Paws", tagline: "AAF Create Event", date:"October 16 - 18, 2020", tools:"Figma, Illustrator, & Photoshop", deliverables:"Posters, trifold, & social media posts", description:"This was a 24hr event put on by the American Advertising Federation in Spokane. I designed collateral for the Spokane Humane Society's event, Parade of Paws in a team of 4. Although my team's design was not chosen, the day after the event I redesigned some of the collateral after receiving feedback from local professionals at the event.", thumb: "/images/print/paradeofpaws/thumb.jpg", img1:"", img2:"", img3:"", portpage:"paradeOfPaws.html", link1:"https://www.figma.com/file/0ufYoXg66d8aqWiKPB8uMn/AFF-Create-2020?node-id=0%3A1", link2:"", link3:""},
+    {id: 3, title: "Skill Share", tagline: "Timebanking is a time-based currency. Give one hour of service and receive one time credit. ", client:"Airway Hieghts Timebank ", date:"April 2018 - May 2018", tools:"Indesign & Illustrator", deliverables:"Design guidelines that include a logo, posters, and trifold options.", description:"Design guidelines for the new Timebank for the Airway Hieghts community. The community needed a logo and design guidlines to apply to posters, handouts, and their timebanking app. ", thumb: "/images/print/skillshare/thumb.jpg", img1:"", img2:"", img3:"", portpage:"skillShare.html", link1:"https://issuu.com/lillian.ragudo/docs/designguidelines", link2:"", link3:""},
+    {id: 4, title: "Fall Blossoms Coloring Book", tagline: "A Coloring book based off of my nature based photography", client:"Self Initiated ", date:"October - November 2020", tools:"Procreate & Indesign", deliverables:"Digital Coloring Book", description:"I created this coloring book first for myself to help me relax. After creating the first 2 pages and sharing them on Instagram I realized that I had friends who would also enjoy something to help them relax. A common problem that myself and my friends usually had with coloring books for adults was that they can be so intricate and detailed that you don't even want to start or finish them. In the end many are the opposite of relaxing. So I purposely made the pages simple enough to not be overwhelmed, but complicated enough so that it did not feel like a children's coloring book.", thumb: "/images/print/coloringBook/thumb.jpg", img1:"", img2:"", img3:"", portpage:"coloringBook.html", link1:"https://www.lillianrosedesign.com/fallBlossoms.pdf", link2:"", link3:""},
+    {id: 5, title: "FBC Internship", tagline: "", client:"Faith Bible Church", date:"January 2020 - March 2020", tools:"Illustrator, Indesign, & After Effects", deliverables:"", description:"I worked on several projects with FBC’s Communication Director, Seth Weber. My work included motion graphics, booklet covers, logo and illustrations for the Youth Winter Camp, and illustrations for Sermons and handouts.", thumb: "/images/print/fbc/thumb.jpg", img1:"", img2:"", img3:"", portpage:"fbc.html", link1:"https://issuu.com/lillian.ragudo/docs/internship_portfolio", link2:"", link3:""}
+]
+
+var illustrationLists = [
+    {id: 1, title: "Wild Ink Designs", tagline: "A website to sell my sticker designs.", client:"Self Initiated", date:"November 2018 - Present", tools:"Squarespace, Figma, & Procreate", deliverables:"Ecommerce website & Brand design", description:"Brand and design my own business for selling my stickers online. I started selling in person at art shows in 2018 on and off. This last summer I decided to officially open my own business to sell my stickers.", thumb: "/images/illustration/wildink/thumb.jpg", img1:"", img2:"", img3:"", portpage:"wildink.html", link1:"https://www.wildinkdesigns.com/", link2:"", link3:""},
+    {id: 2, title: "Posters", tagline: "", client:"Self Initiated", date:"October - December 2018", tools:"Illustrator & Procreate", deliverables:"Posters for my art shows", description:"I designed these posters for my first art show in 2018. I continue to sell prints of them when I attend art shows and night markets during the summers.", thumb: "/images/illustration/posters/thumb.jpg", img1:"", img2:"", img3:"", portpage:"posters.html", link1:"", link2:"", link3:""},
+    {id: 3, title: "", tagline: "", client:"Self Initiated", date:"", tools:"", deliverables:"", description:"", thumb: "/images/web/***/thumb.jpg", img1:"", img2:"", img3:"", portpage:"*****.html", link1:"", link2:"", link3:""}
+]
+
+
+  
+  function Section({items, name}){
+    return(
+      <>
+        <h1>{name}</h1>
+        {items.map(item => (
+             <section key={item.id}>
+               <a class="portItem" href={item.portpage}>
+                 <div class="info">
+                    <img src={item.image}></img>
+                    <h4>{item.title}</h4>
+                 </div>
+                </a>
+             </section> 
+        ))}
+      </>
+    )
+  }
+  
+  ReactDOM.render(
+     <Section
+       items={webLists, "Web & UX"}
+       />,
+    document.getElementById("app")
+  );
+  
